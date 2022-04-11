3b9360aec82424eb4365cb19235e5f464eaf645f
HoangDuck <hoanghuuduc22@gmail.com>
2022-04-11 08:47:06 +0700

    project structure

--- app/src/main/java/hcmute/edu/vn/foody_08/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/foody_08/MainActivity.java
@@ -2,7 +2,11 @@ package hcmute.edu.vn.foody_08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import hcmute.edu.vn.foody_08.view.LoginRegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openPage(View view) {
+        Intent intent=new Intent(MainActivity.this, LoginRegisterActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/hcmute/edu/vn/foody_08/view/LoginRegisterActivity.java
@@ -0,0 +1,16 @@
+package hcmute.edu.vn.foody_08.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hcmute.edu.vn.foody_08.R;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+}
